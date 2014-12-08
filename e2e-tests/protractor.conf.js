4424f889f87095019e34fb61446e136398d24a13
jo <jo@ubuntu14.04>
2014-12-08 15:57:34 +0900

    update configuratin so headless e2e test works properly.

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,19 +1,28 @@
 exports.config = {
   allScriptsTimeout: 11000,
+  
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
   specs: [
     '*.js'
   ],
 
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'phantomjs',
+    'phantomjs.binary.path': require('phantomjs').path,
+    'phantomjs.cli.args': ['--webdriver=8910', '--webdriver-loglevel=DEBUG', '--webdriver-logfile=phantomjs.log']
   },
+
+  chromeOnly: false,
 
   baseUrl: 'http://localhost:8000/app/',
 
   framework: 'jasmine',
 
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+      isVerbose: true,
+      showColors: true,
+      includeStackTrace: true,
+      defaultTimeoutInterval: 30000
   }
 };
